8f9fb6edff091b43fcd1bd57a87641b5bb5618b2
Yatin Chaudhari <101591532+YatinDevs@users.noreply.github.com>
2022-03-16 23:03:52 +0530

    Add files via upload

---
+++ TestType.java
@@ -0,0 +1,23 @@
+package Basiccommands;
+
+public class TestType {
+  boolean b;
+  int a;
+  float c=90;
+  double d;
+	public static void main(String[] args) 
+	{ 
+	  float r= 89.8f; // by default decimal is considered as double
+	  
+		
+	 TestType t= new TestType();
+	 System.out.println(t.b);
+	 System.out.println(t.a);
+	 System.out.println(t.c);
+	 System.out.println(t.d);
+	 System.out.println(r);
+	 
+	
+	}
+
+}
