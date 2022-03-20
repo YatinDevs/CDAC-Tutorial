262a9c0420cf88c302b7dfb6ba3d9d7509f8ec29
Yatin Chaudhari <101591532+YatinDevs@users.noreply.github.com>
2022-03-20 19:43:41 +0530

    Add files via upload

---
+++ Q3CubeOfNumbers.java
@@ -0,0 +1,20 @@
+package Loopassignment3;
+
+public class Q3CubeOfNumbers {
+
+	public static void main(String[] args) 
+	{
+		
+		for(int n=1;n<=5;n++)
+		{
+			
+			System.out.println("Cube Of" + "Number :" + n + " is :" + n*n*n);
+			
+		}
+		
+		
+
+	}
+
+}
+
